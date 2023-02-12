f091a32bbbaaf790706a005ded6c31946d2cd704
Adam Tegelberg Hägnefors <ad3444te-s@student.lu.se>
2023-02-12 19:03:44 +0100

    lade till paket och key/truststores och gitignore

---
+++ server/User.java
@@ -0,0 +1,16 @@
+package server;
+
+class User {
+    String username;
+    String password;
+    String type;
+  
+
+    public User(String username, String password, String type) {
+        this.username = username;
+        this.password = password;
+        this.type = type;
+    }
+
+    
+}
