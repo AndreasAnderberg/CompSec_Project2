91d24da140bbf2d14470a49e3772a55a11a97ac4
Adam Tegelberg Hägnefors <ad3444te-s@student.lu.se>
2023-02-25 18:45:55 +0100

    a

---
+++ Project_2/server/Record.java
@@ -0,0 +1,69 @@
+package Project_2.server;
+
+import java.io.*;
+
+public class Record {
+  private String patientName;
+  private String doctor;
+  private String nurse;
+  private String division;
+
+  public Record(String patientName, String doctor, String nurse, String division) {
+    this.patientName = patientName;
+    this.doctor = doctor;
+    this.nurse = nurse;
+    this.division = division;
+  }
+
+  public String getPatientName() {
+    return patientName;
+  }
+
+  public String getDoctor() {
+    return doctor;
+  }
+
+  public String getNurse() {
+    return nurse;
+  }
+
+  public void saveToFile(String filename) throws IOException {
+    File file = new File("records/" + filename);
+    FileWriter writer = new FileWriter(file);
+    writer.write(patientName + "\n");
+    writer.write(doctor + "\n");
+    writer.write(nurse + "\n");
+    writer.write(division + "\n");
+    writer.close();
+  }
+
+  public static Record loadFromFile(String filename) throws IOException {
+    File file = new File(filename);
+    BufferedReader reader = new BufferedReader(new FileReader(file));
+    String patientName = reader.readLine();
+    String doctor = reader.readLine();
+    String nurse = reader.readLine();
+    String division = reader.readLine();
+    reader.close();
+    return new Record(patientName, doctor, nurse, division);
+  }
+
+  public static Record readRecord(String filename) throws IOException {
+    try {
+        FileReader fileReader = new FileReader(filename);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String patientName = bufferedReader.readLine();
+        String doctor = bufferedReader.readLine();
+        String nurse = bufferedReader.readLine();
+        String division = bufferedReader.readLine();
+        Record record = new Record(patientName, doctor, nurse, division);
+        fileReader.close();
+        return record;
+
+    } catch (IOException e) {
+        return null;
+    }
+  } 
+
+}
+
